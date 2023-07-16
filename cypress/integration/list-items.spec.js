556463ba76e3e2f2268ab65c1a6c46b8d6a47822
konfle <konrad.flejter@gmail.com>
2023-07-16 09:50:40 +0200

    Test for removing todo form todo list

--- cypress/integration/list-items.spec.js
+++ cypress/integration/list-items.spec.js
@@ -13,8 +13,30 @@ describe("List items", () => {
         cy.contains(".todo-list li.completed", "Eggs")
     })
 
-    it.only("Shows reaming todos in the footer", () => {
+    it("Shows reaming todos in the footer", () => {
         cy.get(".todo-count")
           .should("contain", 3)
+    })
+
+    it.only("Removes a todo", () => {
+        cy.route({
+            url: "/api/todos/1",
+            method: "DELETE",
+            status: 200,
+            response: {}
+        })
+
+        cy.get(".todo-list li")
+            .as("list")
+        
+        cy.get("@list")
+            .first()
+            .find(".destroy")
+            .invoke("show", )
+            .click()
+
+        cy.get("@list")
+            .should("have.length", 3)
+            .and("not.contains", "Milk")
     })
 })
