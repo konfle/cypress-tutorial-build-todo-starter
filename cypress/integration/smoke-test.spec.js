ff2c8682ee5ddf6a45dd60a4db21971432f90c28
konfle <konrad.flejter@gmail.com>
2023-07-23 17:52:37 +0200

    Somke test case add one todo item

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -10,3 +10,22 @@ describe("The application loads", () => {
       cy.get(".todo-list").should("exist");
     });
 });
+
+describe("Smoke tests", () => {
+  beforeEach(() => {
+    cy.request("GET", "/api/todos")
+      .its("body")
+      .each(todo => cy.request("DELETE", `/api/todos/${todo.id}`))
+  })
+
+  context("With no todos", () => {
+    it.only("Saves new todos", () => {
+      cy.visit("/")
+      cy.focused()
+        .type("Buy milk{enter}")
+
+      cy.get(".todo-list li")
+        .should("have.length", 1)
+    })
+  })
+})
