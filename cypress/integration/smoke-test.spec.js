7deef7f5b1b123e8e01f8615ad654e99cfc188ce
konfle <konrad.flejter@gmail.com>
2023-07-23 18:11:16 +0200

    Refactor test to add miltuples items

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -20,18 +20,27 @@ describe("Smoke tests", () => {
 
   context("With no todos", () => {
     it.only("Saves new todos", () => {
+      const items = [
+        {text: "Buy milk", expectedLength: 1},
+        {text: "Buy eggs", expectedLength: 2},
+        {text: "Buy bread", expectedLength: 3}
+      ]
       cy.visit("/")
       cy.server()
       cy.route("POST", "/api/todos")
         .as("create")
 
-      cy.focused()
-        .type("Buy milk{enter}")
+      cy.wrap(items)
+        .each(todo => {
+        cy.focused()
+          .type(todo.text)
+          .type("{enter}")
 
-      cy.wait("@create")
+        cy.wait("@create")
 
-      cy.get(".todo-list li")
-        .should("have.length", 1)
+        cy.get(".todo-list li")
+          .should("have.length", todo.expectedLength)
+        })
     })
   })
 })
