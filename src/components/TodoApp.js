3a42f6b65f7f3f240f54cffb79ffc87bda2fa29c
konfle <konrad.flejter@gmail.com>
2023-07-23 18:02:41 +0200

    Removed server delay

--- src/components/TodoApp.js
+++ src/components/TodoApp.js
@@ -56,14 +56,12 @@ handleToggle (id) {
 handleTodoSubmit (evt) {
   evt.preventDefault()
   const newTodo = {name: this.state.currentTodo, isComplete: false}
-  setTimeout(() => {
-    saveTodo(newTodo)
-    .then(({data}) => this.setState({
-      todos: this.state.todos.concat(data),
-      currentTodo: " "
-    }))
-    .catch(() => this.setState({error: true}))
-  }, 4500)
+  saveTodo(newTodo)
+  .then(({data}) => this.setState({
+    todos: this.state.todos.concat(data),
+    currentTodo: " "
+  }))
+  .catch(() => this.setState({error: true}))
   
 }
 
