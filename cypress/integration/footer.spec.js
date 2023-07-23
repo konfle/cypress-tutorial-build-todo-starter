cdb6539e0adace11a6527a2c5fd8e26e63ce3592
konfle <konrad.flejter@gmail.com>
2023-07-23 16:58:53 +0200

    Test case for filter actives todo

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -18,5 +18,13 @@ describe("Footer", () => {
             cy.get(".todo-count")
               .should("contain", "3 todos left")
         })
+
+        it.only("Filters to active todos", () => {
+            cy.contains("Active")
+              .click()
+
+            cy.get(".todo-list li")
+              .should("have.length", 3)
+        })
     })
 })
