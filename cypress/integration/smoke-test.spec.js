b8f28d187a38da27cd5536cd18ae5900da285f81
konfle <konrad.flejter@gmail.com>
2023-07-23 18:22:26 +0200

    Test case for load the data from DB

--- cypress/integration/smoke-test.spec.js
+++ cypress/integration/smoke-test.spec.js
@@ -19,7 +19,7 @@ describe("Smoke tests", () => {
   })
 
   context("With no todos", () => {
-    it.only("Saves new todos", () => {
+    it("Saves new todos", () => {
       const items = [
         {text: "Buy milk", expectedLength: 1},
         {text: "Buy eggs", expectedLength: 2},
@@ -41,6 +41,22 @@ describe("Smoke tests", () => {
         cy.get(".todo-list li")
           .should("have.length", todo.expectedLength)
         })
+    })
+  })
+
+  context("With active todos", () => {
+    beforeEach(() => {
+      cy.fixture("todos")
+        .each(todo => {
+          const newTodo = Cypress._.merge(todo, {isComplete: false})
+          cy.request("POST", "/api/todos", newTodo)
+        })
+      cy.visit("/")
+    })
+
+    it("Loads existing data from the DB", () => {
+      cy.get(".todo-list li")
+        .should("have.length", 4)
     })
   })
 })
