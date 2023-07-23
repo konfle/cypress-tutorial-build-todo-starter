e3a14b34588cdbd76a83d9d70e748f1b5eec3e39
konfle <konrad.flejter@gmail.com>
2023-07-23 16:46:13 +0200

    Added footer test case for single todo

---
+++ cypress/integration/footer.spec.js
@@ -0,0 +1,11 @@
+describe("Footer", () => {
+    context("With a single todo", () => {
+        it.only("Displays a singular todo in count", () => {
+            cy.seedAndVisit([{id: 1, 
+                              name:"Buy milk", 
+                              isComplete: false}])
+            cy.get(".todo-count")
+              .should("contain", "1 todo left")
+        })
+    })
+})
