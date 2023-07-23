9ea84f95375d11ecc32fab5c9bf50266f087272a
konfle <konrad.flejter@gmail.com>
2023-07-23 16:52:15 +0200

    Added footer test in case of multiple todos

--- cypress/integration/footer.spec.js
+++ cypress/integration/footer.spec.js
@@ -1,11 +1,20 @@
 describe("Footer", () => {
     context("With a single todo", () => {
-        it.only("Displays a singular todo in count", () => {
+        it("Displays a singular todo in count", () => {
             cy.seedAndVisit([{id: 1, 
                               name:"Buy milk", 
                               isComplete: false}])
             cy.get(".todo-count")
               .should("contain", "1 todo left")
+        })
+    })
+
+    context("With multiple todos", () => {
+        it.only("Dispalys plural todos in count", () => {
+            cy.seedAndVisit()
+
+            cy.get(".todo-count")
+              .should("contain", "3 todos left")
         })
     })
 })
